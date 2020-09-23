6c885ad218fc13fd5e2eee664dee1b773ad618d9
Jong Hwan Na <45410162+Jhna246@users.noreply.github.com>
2020-09-23 09:50:33 +0900

    Create layout-campaign.js

---
+++ coinraiser/component/layout-campaign.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './header-campaign'
+import Head from 'next/head';
+
+export default (props) => {
+    return(
+        <div>
+            <style jsx>{`
+                div {
+                    font-family: 'Balsamiq Sans', cursive;
+                }
+            `}
+            </style>
+            <title>Coinraiser</title>
+            <Head>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+                <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.css"/>
+                <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet" />
+            </Head>
+
+            <Header />
+            {props.children}
+        </div>
+    )
+};
